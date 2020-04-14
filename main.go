3e0e0aaf8ed8bbd033183eeb2d7b6172a68bb0d4
arroyo-piedra <andre.arroyo.piedra@gmail.com>
2020-04-14 16:02:17 -0600

    Struct

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	struct "github.com/arroyo-piedra/first-app/Struct"
 )
 
 func main() {
 	fmt.Println("Hello World")
-
+	p := struct.newPerson("Juan")
+	fmt.Println(p.name)
 }
