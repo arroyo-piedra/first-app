1ba11765e26be7538b1209c6ab13f35b1ccf013f
arroyo-piedra <andre.arroyo.piedra@gmail.com>
2020-04-14 18:20:54 -0600

    Examples

--- main.go
+++ main.go
@@ -2,11 +2,71 @@ package main
 
 import (
 	"fmt"
-	struct "github.com/arroyo-piedra/first-app/Struct"
+	"sync"
+	"time"
 )
 
+type character interface {
+	attack() string
+	defense() string
+}
+
+type knight struct {
+	swordType   string
+	swordWeight int
+}
+
+func (k knight) attack() string {
+	return "Use a sword"
+}
+func (k knight) defense() string {
+	return "Use a shield"
+}
+
+type archer struct {
+	bow string
+}
+
+func (a archer) attack() string {
+	return "Use a bow"
+}
+func (a archer) defense() string {
+	return "Dodge"
+}
+
+func characterMoves(c character) {
+	fmt.Println(c)
+	fmt.Println(c.attack())
+	fmt.Println(c.defense())
+}
+
 func main() {
-	fmt.Println("Hello World")
-	p := struct.newPerson("Juan")
-	fmt.Println(p.name)
+	knight := knight{swordType: "normal", swordWeight: 5}
+	archer := archer{bow: "wood"}
+
+	fmt.Println("This knight: ")
+	characterMoves(knight)
+	fmt.Println("This archer: ")
+	characterMoves(archer)
+
+	count("cat")
+	count("dog")
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		count("sheeps")
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+}
+
+func count(animal string) {
+	for i := 1; true; i++ {
+		fmt.Println(i, animal)
+		time.Sleep(time.Millisecond * 500)
+	}
 }
